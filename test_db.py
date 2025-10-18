b9f0dcac5ff43279f8fc49baa67e96ad77f611d9
yanjie0312 <e1349375@u.nus.edu>
2025-10-18 20:49:30 +0800

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -1,17 +1,8 @@
-@@ -1,13 +1,16 @@
 from sqlalchemy import create_engine, text
 from config import settings
-# db.py （示例）
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 # 创建数据库引擎
 engine = create_engine(settings.DATABASE_URL)
-DB_USER = os.environ["DB_USER"]
-DB_PASS = os.environ["DB_PASS"]
-DB_NAME = os.environ["DB_NAME"]
-INSTANCE_CONNECTION_NAME = os.environ["INSTANCE_CONNECTION_NAME"]  # 形如 project:region:instance
 
 try:
     with engine.connect() as conn:
@@ -20,9 +11,3 @@ try:
         print("PostgreSQL 版本：", result[0])
 except Exception as e:
     print("❌ 数据库连接失败：", e)
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@/{DB_NAME}"
-    f"?host=/cloudsql/{INSTANCE_CONNECTION_NAME}&sslmode=disable"
-)
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
